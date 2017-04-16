f21a70d50b2b2089b9cfabd7920c0149da93115d
хэланир Санскар < >
2017-04-16 21:38:56 +0300

    MoveController xy test terrain test char

---
+++ Random Shooter/Assets/Scripts/MoveController.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MoveController : MonoBehaviour
+{
+
+
+    public float moveSpeed, strafeSpeed;
+    private float move, strafe;
+    private Rigidbody rigidbody;
+
+    void Start()
+    {
+        rigidbody = GetComponent<Rigidbody>();
+
+    }
+
+    void FixedUpdate()
+    {
+        Move();
+        Strafe();
+
+    }
+
+    private void Move()
+    {
+        Vector3 movement = transform.forward * move * moveSpeed * Time.deltaTime;
+        rigidbody.MovePosition(rigidbody.position + movement);
+    }
+    private void Strafe()
+    {
+        Vector3 strafing = transform.right * strafe * strafeSpeed * Time.deltaTime;
+        rigidbody.MovePosition(rigidbody.position + strafing);
+    }
+    private void Update()
+    {
+        move = Input.GetAxis("Vertical");
+        strafe = Input.GetAxis("Horizontal");
+    }
+   
+}
